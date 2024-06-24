0d7d90fa193f7b1caa92ac6a8290d912e1c9ecba
Megan Noel <144546725+megannoeln@users.noreply.github.com>
2024-06-24 18:03:49 -0400

    uploading files

---
+++ web2/web2/Controllers/AboutUsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web2.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            Models.User u = new Models.User();
+
+            u.FirstName = "Megan";
+            u.LastName = "Noel";
+            u.UserID = "mnoel";
+            u.Password = "mnoel8";
+            u.Email = "mnoel@gmail";
+
+           return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection col) {
+
+            if (col["btnSubmit"] == "more") {               
+                return RedirectToAction("More", "AboutUs");
+            }
+
+            if (col["btnSubmit"] == "close") {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            return View();
+        }
+
+
+        public ActionResult More() {
+            Models.User u = new Models.User();
+
+            u.FirstName = "Megan";
+            u.LastName = "Noel";
+            u.UserID = "mnoel";
+            u.Password = "mnoel8";
+            u.Email = "mnoel@gmail";
+
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult More(FormCollection col) {
+            return RedirectToAction("Index", "AboutUs");
+        }
+
+
+
+
+    }
+}
